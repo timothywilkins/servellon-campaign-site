90d3361304c5fd1fd1161c9d8c5d21ed88df7f31
Tim Wilkins <tim.f.wilkins@gmail.com>
2019-07-18 14:11:53 -0500

    fix logo, make responsive, autoprefixed and babeled

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+"use strict";
 
-
-  const heading = document.querySelector(".heading");
-  const donate = document.querySelector(".donate")
-  const fab = document.querySelectorAll(".fab");
-  
-  var tl = new TimelineLite;
-  
-  tl.from(heading,0.9, {left:"-=60px", ease:Back.easeOut})
-  .staggerFrom("rect",0.7,{width:0,ease:Back.easeOut},0.5, "-=0.8")
-    .from(donate, 0.9, {autoAlpha:0}, "-=0.7")
-    .staggerFrom(fab, .7 ,{top:"-=30px", rotation:"-40deg", autoAlpha:0, scale:1.8, ease:Back.easeOut}, 0.2);
+var heading = document.querySelector(".heading");
+var donate = document.querySelector(".donate");
+var fab = document.querySelectorAll(".fab");
+var subHeading = document.querySelector(".logo h2");
+var tl = new TimelineLite();
+tl.from(heading, 0.9, {
+  left: "-=60px",
+  ease: Back.easeOut
+}).staggerFrom("rect", 0.7, {
+  width: 0,
+  ease: Back.easeOut
+}, 0.5, "-=0.8").from(donate, 0.9, {
+  autoAlpha: 0
+}, "-=0.7").from(subHeading, 0.9, {
+  autoAlpha: 0
+}, "-=0.7").staggerFrom(fab, .7, {
+  top: "-=30px",
+  rotation: "-40deg",
+  autoAlpha: 0,
+  scale: 1.8,
+  ease: Back.easeOut
+}, 0.2);
